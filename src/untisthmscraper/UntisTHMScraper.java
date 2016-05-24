62b5e67b8efa9c4f7e4b8efd0bc6d66f79abcd70
AppleFoxUser42 <fabian_otto_de@yahoo.de>
2016-05-24 22:21:13 +0200

    got Main Window to work on Mac. Changed normal instantiation of UI in Main Class to invoke.later-thread-thing.

--- src/untisthmscraper/UntisTHMScraper.java
+++ src/untisthmscraper/UntisTHMScraper.java
@@ -1,9 +1,11 @@
+package untisthmscraper;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package untisthmscraper;
+
 
 import java.io.IOException;
 import java.util.logging.Level;
@@ -16,11 +18,13 @@ import untisthmscraper.UI.UI;
  */
 public class UntisTHMScraper {
     public static void main(String[] args){
-        try {
-            UI ui = new UI();
-        } catch (IOException ex) {
-            Logger.getLogger(UntisTHMScraper.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
+        java.awt.EventQueue.invokeLater(() -> {
+            try {
+                new UI().setVisible(true);
+            } catch (IOException ex) {
+                Logger.getLogger(UntisTHMScraper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        
     }
 }
